3e809653ce896de787c6e7b001050ed53003f2f2
jimcreel <jim.creel@gmail.com>
2023-03-10 18:21:18 -0800

    removed old AWS API for straight request from Disney. we'll see how long they keep it active.

--- models/api.js
+++ models/api.js
@@ -4,10 +4,9 @@ const axios = require('axios')
 module.exports = {
     getResorts: async function (req, res) {
         console.log('getResorts function called')
-        const DLRresorts = await axios.get(`${process.env.DLRURL}`)
-        const WDWresorts = await axios.get(`${process.env.WDWURL}`)
+        const DLRresorts = await axios.get('https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=inspire-key-pass,believe-key-pass,enchant-key-pass,imagine-key-pass,dream-key-pass&destinationId=DLR&numMonths=14')
+        const WDWresorts = await axios.get('https://disneyworld.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=disney-incredi-pass,disney-sorcerer-pass,disney-pirate-pass,disney-pixie-dust-pass&destinationId=WDW&numMonths=13')
         const allData = DLRresorts.data.concat(WDWresorts.data)
-        console.log(typeof DLRresorts.data)
         return allData
     }
 }
