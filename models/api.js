3aad8bbac595b2dc5ecb3bb57c91c95a8e931406
Jim <jim.creel@gmail.com>
2023-03-14 21:13:49 -0700

    moved resort URLS to .env, function only returns one array now depending on selection

--- models/api.js
+++ models/api.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 const axios = require('axios')
 
 module.exports = {
-    getResorts: async function (req, res) {
-        const DLRresorts = await axios.get('https://disneyland.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=inspire-key-pass,believe-key-pass,enchant-key-pass,imagine-key-pass,dream-key-pass&destinationId=DLR&numMonths=14')
-        const WDWresorts = await axios.get('https://disneyworld.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=disney-incredi-pass,disney-sorcerer-pass,disney-pirate-pass,disney-pixie-dust-pass&destinationId=WDW&numMonths=13')
-        const allData = DLRresorts.data.concat(WDWresorts.data)
+    getResorts: async function (req, res, resort) {
+        const resorts = await axios.get(resort)
+        
         return allData
     }
 
