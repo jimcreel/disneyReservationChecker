1ada4c860a1c449b521e2ad41ed9e9609b8fd747
jimcreel <jim.creel@gmail.com>
2023-05-26 18:33:59 -0700

    updated deployment settings

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://magicreservations.jim-creel.com:3000',
                 changeOrigin: true,
                 secure: false,
                 ws: true,
