ebb41cf2e164fe8bc41969c1cb62e4a0b02cf5ca
Jim Creel <jim.creel@gmail.com>
2023-05-28 10:55:41 -0700

    proxy requests to backend

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,22 @@ import 'dotenv/config';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     root: path.resolve(__dirname, 'frontend'),
     envFile: path.resolve(__dirname, '.env'),
-    server: {
-        proxy: {
-            '/api': {
+        server: {
+            proxy: {
+              '/api': {
                 target: 'https://magicres-backend.herokuapp.com',
                 changeOrigin: true,
                 secure: false,
                 ws: true,
-            }
-        }
-    }
+              },
+            },
+          },
 })
 
