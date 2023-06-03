b031812113d7e95f4186e3b18d8e8eb05e44ed7d
jimcreel <jim.creel@gmail.com>
2023-06-03 10:42:36 -0700

    added change-password route

--- frontend/utils/backend.jsx
+++ frontend/utils/backend.jsx
@@ -65,7 +65,7 @@ export async function editUser(user){
 
 export async function changePassword(user){
     let headers = buildHeader()
-    const response = await axios.put(`${uri}/api/users/`, user, headers);
+    const response = await axios.put(`${uri}/api/users/change-password`, user, headers);
     // const response = await axios.put(`http://localhost:3000/api/users/password`, user, headers);
     console.log(response.data)
     return response.data;
